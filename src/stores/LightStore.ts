26a68613407ca6c3e9a97ffbcd3d93863af7e0b3
Frank Oppervoll-Larsen <frlarsen@microsoft.com>
2021-02-19 22:47:43 +0100

    Fetch hub address from discovery service

--- src/stores/LightStore.ts
+++ src/stores/LightStore.ts
@@ -4,10 +4,16 @@ import { readable } from "svelte/store";
 import { hueUserId } from "../config";
 import type { HueLight } from "../types/HueLight";
 
-type HueLightContainer = { [key: string]: HueLight };
+let hubAddress: string | undefined = undefined;
 
+type HueLightContainer = { [key: string]: HueLight };
 async function fetchLightData(): Promise<HueLightContainer> {
-  const url = `http://192.168.10.112/api/${hueUserId}/lights`;
+  if (!hubAddress) {
+    const hubData = await fetch("https://discovery.meethue.com/");
+    const hubJsonData = await hubData.json();
+    hubAddress = hubJsonData[0]["internalipaddress"];
+  }
+  const url = `http://${hubAddress}/api/${hueUserId}/lights`;
   const lightData = await fetch(url);
   const jsonResponse = await lightData.json();
   return jsonResponse as HueLightContainer;
@@ -15,8 +21,12 @@ async function fetchLightData(): Promise<HueLightContainer> {
 
 export const lights = readable<HueLightContainer>({}, (set) => {
   const interval = setInterval(() => {
-    fetchLightData().then((lights) => {
+    fetchLightData()
+      .then((lights) => {
       set(lights);
+      })
+      .catch((error) => {
+        console.log(error);
     });
   }, 5000);
 
