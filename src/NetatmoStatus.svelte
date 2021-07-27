527aaff0c7c91982e9ce9a8cdb8df52119e936d2
Frank Oppervoll-Larsen <frank@oppervoll-larsen.no>
2021-07-28 00:41:08 +0200

    svelte plugin for prettier, tweaks to netatmo

--- src/NetatmoStatus.svelte
+++ src/NetatmoStatus.svelte
@@ -1,18 +1,55 @@
 <script lang="ts">
- import {raw,sensors} from "./stores/NetatmoStore";
- export let plan: number = 0;
- $: temp = $sensors[plan]?.temperature.current;
- $: hum = $sensors[plan]?.humidity;
- $: co2 = $sensors[plan]?.co2;
-
+  import { raw, sensors } from "./stores/NetatmoStore";
+  export let plan: number = 0;
+  $: temp = $sensors[plan]?.temperature.current;
+  $: hum = $sensors[plan]?.humidity;
+  $: co2 = $sensors[plan]?.co2;
 </script>
 
-<div>
-    <span>{temp}°C, {hum}%, {co2}ppm</span>
+<div class="netatmo">
+  <div class="temp">
+    <h3>Temperatur</h3>
+    <div class="line">
+      <span class="measure">{temp}</span><span class="unit">°C</span>
+    </div>
+  </div>
+  <div class="humidity">
+    <h3>Humidity</h3>
+    <div class="line">
+      <span class="measure">{hum}</span><span class="unit">%</span>
+    </div>
+  </div>
+  <div class="co2">
+    <h3>CO<sub>2</sub></h3>
+    <div class="line">
+      <span class="measure">{co2}</span><span class="unit">ppm</span>
+    </div>
+  </div>
 </div>
 
-<style>
- div {
-     color: green;
- }
+<style lang="scss">
+  div.netatmo {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  div {
+    h3 {
+      font-size: 60%;
+      margin: 0;
+      padding: 0;
+    }
+  }
+  .temp .line {
+    font-size: 125%;
+  }
+  .line {
+    display: flex;
+    align-items: flex-start;
+  }
+  .measure {
+    font-size: 120%;
+  }
+  .unit {
+    font-size: 60%;
+  }
 </style>
